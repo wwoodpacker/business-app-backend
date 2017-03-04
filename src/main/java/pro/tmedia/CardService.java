f33a5c9d39734598eb08c2e9748d09dab8963c59
ivaykin <t.s.ivaykin@gmail.com>
2017-03-04 17:32:23 +1000

    public constructor for CardService

--- src/main/java/pro/tmedia/CardService.java
+++ src/main/java/pro/tmedia/CardService.java
@@ -8,7 +8,7 @@ import java.util.List;
  */
 public class CardService {
     List<Card> allCards = new ArrayList<>();
-    CardService() {
+    public CardService() {
         int id = 0;
         Card card;
 
