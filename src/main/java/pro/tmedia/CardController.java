0cd9673f3159a6c7fc0e4e568685b1df8ba66f29
Timothy S. Ivaykin M.E <t.s.ivaykin@gmail.com>
2017-03-12 18:41:15 +1000

    CardsCRUD init and JSON mime

--- src/main/java/pro/tmedia/CardController.java
+++ src/main/java/pro/tmedia/CardController.java
@@ -1,7 +1,18 @@
 package pro.tmedia; /**
  * Created by TIvaykin on 04.03.2017.
  */
+import com.heroku.sdk.jdbc.DatabaseUrl;
+import spark.ModelAndView;
 import spark.Spark;
+import spark.template.freemarker.FreeMarkerEngine;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import static pro.tmedia.JsonUtil.*;
 import static spark.Spark.after;
@@ -11,9 +22,36 @@ public class CardController {
 
     public CardController(final CardService cardService) {
 
-        get("/cards", (req, res) -> cardService.getAllCards(), JsonUtil.json());
-        after((req, res) -> {
+        get("/cards", (req, res) -> {
             res.type("application/json");
-        });
+            return cardService.getAllCards();
+        }, JsonUtil.json());
+
+
+        get("/cardsCRUD", (req, res) -> {
+            Connection connection = null;
+            Map<String, Object> attributes = new HashMap<>();
+            try {
+                connection = DatabaseUrl.extract().getConnection();
+
+                Statement stmt = connection.createStatement();
+                stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+                stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+                ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+
+                ArrayList<String> output = new ArrayList<String>();
+                while (rs.next()) {
+                    output.add( "Read from DB: " + rs.getTimestamp("tick"));
+                }
+
+                attributes.put("results", output);
+                return new ModelAndView(attributes, "depot.ftl");
+            } catch (Exception e) {
+                attributes.put("message", "There was an error: " + e);
+                return new ModelAndView(attributes, "error.ftl");
+            } finally {
+                if (connection != null) try{connection.close();} catch(SQLException e){}
+            }
+        }, new FreeMarkerEngine());
     }
 }
